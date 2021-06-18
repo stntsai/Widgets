5754430b04e57e46c1ae770a0610c548eb267d6b
Steven <stntsai93@gmail.com>
2021-06-18 14:40:43 +0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default ()=>{
 
-export default App;
+    const items=[
+        {
+            title: 'what is React?',
+            content: 'React is a front end javascript framework'
+        },
+        {
+            title: 'Hello!',
+            content: 'how are you?'
+        },
+        {
+            title: 'Who are you?',
+            content: 'No one'
+        }
+    ]
+
+    return (
+        <div>
+            <Accordion items = {items}/>
+        </div>
+    )
+};
