582eb35a4d9993efba04a527eac583a8d80b7765
Steven <stntsai93@gmail.com>
2021-06-21 14:28:17 +0800

    dropdown update some jsx

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from  'axios';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 
+const items=[
+    {
+        title: 'what is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Hello!',
+        content: 'how are you?'
+    },
+    {
+        title: 'Who are you?',
+        content: 'No one'
+    }
+]
+
+const options=[
+    {
+        label:'The Color Red',
+        value:'red'
+    },
+    {
+        label:'The Color Green',
+        value:'green'
+    },
+    {
+        label:'A Shade Of Blue',
+        value:'blue'
+    }
+]
+
+
+    
+
 export default ()=>{
-
-    const items=[
-        {
-            title: 'what is React?',
-            content: 'React is a front end javascript framework'
-        },
-        {
-            title: 'Hello!',
-            content: 'how are you?'
-        },
-        {
-            title: 'Who are you?',
-            content: 'No one'
-        }
-    ]
-
+    const [selected, setSelected] = useState(options[0])
     return (
         <div className="ui container">
-            <Dropdown />
+            <Dropdown selected={selected} onSelectedChange={setSelected} options={options}/>
         </div>
-    )
+    )    
 };
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
-const Dropdown =()=>{
-    return <h1>Dropdown</h1>
+const Dropdown =({options, selected, onSelectedChange})=>{
+    const renderedOptions = options.map((option)=>{
+        if (option.value!==selected.value){
+            return(
+                <div onClick={()=>onSelectedChange(option)} key={option.value} className="item">
+                    {option.label}
+                </div>
+            )
+        }
+    })
+    return (
+        <div className="ui form">
+            <div className="field">
+                <label className="label">Select a Color</label>
+                <div className="ui selection dropdown visible active">
+                    <i className="dropdown icon"/>
+                    <div className="text">{selected.label}</div>
+                    <div className="menu visible transition">
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+    
 }
 
 export default Dropdown;
